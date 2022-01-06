68f8c0725041ea917981ee5fd93062289aa91c6b
César Farkas <farkascesar@hotmail.com>
2022-01-05 22:53:06 -0300

    composer update / criado migrate tarefas

---
+++ api/database/migrations/2022_01_06_013831_create_tarefas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->text('descricao')->nullable();
+            $table->string('status')->default('ativa');
+            $table->dateTime('data_inclusao');
+            $table->dateTime('data_conclusao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
