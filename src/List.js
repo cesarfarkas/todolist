fda788a8c5e8331dd2aeabf84a5e7f397931dbe7
César Farkas <farkascesar@hotmail.com>
2022-01-06 03:51:05 -0300

    ajustado reder e axios. Ajuste class tarefa margin bottom

--- src/List.js
+++ src/List.js
@@ -1,29 +1,23 @@
-import React, { useState } from "react"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import api from "./Api";
 import './estilos/List.css'
 
-export default class List extends React.Component {
-
-     state = {listaTarefas:[]};
-
-    componentDidMount() {
-
-        axios.get(`http://127.0.0.1:8000/api/tarefas`)
-            .then(res => {
-                const dados = res.data;
-                this.setState((state) => {
-                    // Importante: use `state` em vez de `this.state` quando estiver atualizando.
-                    return {dados}
-                  });
-            })
-
-    }
-
-    render() {
-        return(
-            <div>
-                { this.state.listaTarefas.map(item => <div className="tarefa">{item.titulo}</div>)}
-            </div>
-        );
-    }
-}
+export default function App() {
+    
+    const [listaTarefas, setlistaTarefas] = useState();
+  
+    useEffect(() => {
+      api
+        .get("/tarefas")
+        .then((response) => setlistaTarefas(response.data))
+        .catch((err) => {
+          console.error("ops! ocorreu um erro" + err);
+        });
+    }, []);
+  
+    return (
+        <div>
+            { listaTarefas?.data.map(valor => <div className="tarefa">{valor.titulo}</div>) }
+        </div>
+    );
+  }
